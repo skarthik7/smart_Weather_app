a1cb0c75d2328e4a8e42c30a275b5d62186c24a5
Sriram Karthik Akella <sk.akella7@gmail.com>
2020-09-30 12:00:00 +0300

    final commit

---
+++ app/src/main/java/com/example/whatstheweather/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.whatstheweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    TextView resultTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
+        resultTextView = findViewById(R.id.resultTextView);
+    }
+
+    public void getWeather(View view) {
+        editText.getText().toString();
+        DownloadTask task = new DownloadTask();
+        task.execute("https://openweathermap.org/data/2.5/weather?q="+ editText.getText().toString()+ "&appid=b6907d289e10d714a6e88b30761fae22");
+
+        InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE); //Hide Keyboard line 1
+        mgr.hideSoftInputFromWindow(editText.getWindowToken(),0); //Hide Keyboard line 2
+    }
+
+    public class DownloadTask extends AsyncTask<String,Void,String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+
+                return result;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+
+                Toast.makeText(getApplicationContext(),"Could not find weather!", Toast.LENGTH_SHORT).show();
+
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+
+                String weatherInfo = jsonObject.getString("weather");
+
+                Log.i("Weather content", weatherInfo);
+
+                JSONArray arr = new JSONArray(weatherInfo);
+
+                String message = "";
+
+                for (int i=0; i < arr.length(); i++) {
+                    JSONObject jsonPart = arr.getJSONObject(i);
+                    String main = jsonPart.getString("main");
+                    String description = jsonPart.getString("description");
+
+                    if(!main.equals("") && !description.equals("")) {
+                        message += main + ": " + description + "\r\n";
+                    }
+                }
+
+                if (!message.equals("")) {
+                    resultTextView.setText(message);
+                } else {
+                    Toast.makeText(getApplicationContext(),"Could not find weather!", Toast.LENGTH_SHORT).show();
+                }
+
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(),"Could not find weather!", Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+
+
+            }
+
+        }
+    }
+
+}
